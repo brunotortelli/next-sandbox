2a75060efb8460b4a2a192a786bb56f302e608c7
Bruno Tortelli <brunotortelli@gmail.com>
2021-09-05 18:19:57 -0300

    nova atualização fundo Dark

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
 function Home(){
-    return <h1>Hello World</h1>
-}
+    return (
+    <>
+    <body>
+        <h1>Next configurado</h1>
+    </body>
+    
+
+
+    <style jsx>{`
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    
+    body {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-sizing: border-box;
+        background-color: #1c1c1c;
+      }
+
+      h1{
+          color: white;
+      }
+
+
+    `}</style>
+
+    
+    </>
+    )}
 
 export default Home
